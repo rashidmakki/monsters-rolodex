29b47a1ec6855452358ef4eaf5272aabb12b9556
RASHID MAKKI <https://github.com/rashidmakki>
2020-04-06 01:17:57 +0530

    Updates

---
+++ precache-manifest.3e9f6f2a5abe31a4a8fca1999e29dd56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c093841f80daa3d0d42324a8848b3c9",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "d1fee4e911dc7c11064b",
+    "url": "/monsters-rolodex.git/static/css/main.2f7238ce.chunk.css"
+  },
+  {
+    "revision": "0bea4ddb0927c3285ceb",
+    "url": "/monsters-rolodex.git/static/js/2.392e371a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex.git/static/js/2.392e371a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fee4e911dc7c11064b",
+    "url": "/monsters-rolodex.git/static/js/main.9955887e.chunk.js"
+  },
+  {
+    "revision": "99942859649c0c189aca",
+    "url": "/monsters-rolodex.git/static/js/runtime-main.959aa25a.js"
+  }
+]);
